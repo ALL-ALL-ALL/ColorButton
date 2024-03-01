b05d40df01104c02c8a05039470ae485f296aece
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-01 18:29:48 +0100

    Initial Commit

---
+++ ColorButton/ColorButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorButtonApp.swift
+//  ColorButton
+//
+//  Created by  Ixart on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
